d38b5f5e3fc9934db66b56b2c0716df69f61abf1
doneber <doneber98@gmail.com>
2021-02-09 10:30:13 -0400

    Create new room from the Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
         <div class="col-sm">
           <h2>Create Room</h2>
           <p>Be a host and create a room</p>
-          <form action="#">
+          <form @submit.prevent="submitFormHost">
             <div class="form-group">
               <input
                 type="text"
@@ -28,15 +28,13 @@
                 v-model="nameRoom"
               />
             </div>
-            <button class="btn btn-secondary" @click="createRoom()">
-              Create Room
-            </button>
+            <button type="submit" class="btn btn-secondary">Create Room</button>
           </form>
         </div>
         <div class="col-sm">
           <h2>Join Room</h2>
           <p>Go to a room</p>
-          <form action="#">
+          <form @submit.prevent="submitFormInvite">
             <div class="form-group">
               <input
                 type="text"
@@ -46,9 +44,14 @@
               />
             </div>
             <div class="form-group">
-              <input type="number" class="form-control" placeholder="Room id" />
+              <input
+                type="number"
+                class="form-control"
+                placeholder="Room id"
+                v-model="idRoom"
+              />
             </div>
-            <button class="btn btn-secondary">Join Room</button>
+            <button type="submit" class="btn btn-secondary">Join Room</button>
           </form>
         </div>
       </div>
@@ -64,6 +67,48 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      nameUser: "Im the user",
+      nameRoom: "nameRoom",
+      idRoom: 2,
+    };
+  },
+  methods: {
+    async submitFormInvite() {
+      console.log("Submit form invite");
+    },
+    async submitFormHost() {
+      console.log("Submit form Host");
+      const data = { nameRoom: this.nameRoom };
+      const response = await fetch(`http://localhost:3000/rooms`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((room) => {
+          console.log("ROOM NEW ->", room);
+          const dataUser = { name: this.nameUser, value: -1 };
+          const newUser = fetch(
+            `http://localhost:3000/rooms/${room.id}/users`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(dataUser),
+            }
+          );
+          return newUser;
+        })
+        .then((user) => user.json())
+        .catch((e) => alert("No pudimos enviar tu respuesta :("));
+      console.log("User: ", response);
+    },
+  },
 };
 </script>
 
